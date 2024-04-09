d9ba58ff11148dec60fc99a75d330b7f792e15cc
karanbadhwar <karanbadhwar07@gmail.com>
2024-04-09 01:41:19 -0400

    Docs

--- src/Components/Foreground.jsx
+++ src/Components/Foreground.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import Card from "./Card";
+import Data from "../Data/data.json";
+import { useRef } from "react";
 
 function Foreground() {
-  return <div className="w-full h-full z-[3] fixed top-0 left-0"></div>;
+  const ref = useRef(null);
+  return (
+    <div
+      ref={ref}
+      className="flex gap-5 w-full h-full z-[3] fixed top-0 left-0 flex-wrap p-10"
+    >
+      {Data.map((item, index) => (
+        <Card data={item} key={index} reference={ref} />
+      ))}
+    </div>
+  );
 }
 
 export default Foreground;
